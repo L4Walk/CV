d5ab66892919addf77d4931cb5a3293c4729999f
L4Walk <L4Walk@chuheng.tech>
2024-06-19 00:53:47 +0800

    add ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 import "./globals.css";
 import React from "react";
@@ -23,6 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
+      <GoogleTagManager gtmId="GTM-YKB81HCM47" />
       <body>{children}</body>
       <Analytics />
     </html>
